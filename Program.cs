8018f83ea6ad0aca15f3310316572b6e2946e1e3
ashaelizabethjoseph <ashacep38@gmail.com>
2025-01-03 21:47:57 +0530

    default api mappings

--- Program.cs
+++ Program.cs
@@ -11,8 +11,16 @@ namespace BooksApi
             var app = builder.Build();
 
             //add mappings
+
             //app.MapGet("/", () => "Hello World!");
+
             app.MapControllers();
+
+            //default mapping to /api/books
+            app.MapGet("/", () =>
+            {
+                return Results.Redirect("/api/books");
+            });
             app.Run();
         }
     }
