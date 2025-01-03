e541e52dbaf648ca75895a9d98fac0127288fc8e
ashaelizabethjoseph <ashacep38@gmail.com>
2025-01-03 22:59:13 +0530

    CORS policy added

--- Program.cs
+++ Program.cs
@@ -7,15 +7,28 @@ namespace BooksApi
             var builder = WebApplication.CreateBuilder(args);
             //add services
             builder.Services.AddControllers();
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("MyCors", builder =>
+                {
+                    builder
+                    .WithOrigins("http://localhost:4200")
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+                });
+            });
 
             var app = builder.Build();
+
+            app.UseCors("MyCors");
+
 
             //add mappings
 
             //app.MapGet("/", () => "Hello World!");
 
             app.MapControllers();
-
+            
             //default mapping to /api/books
             app.MapGet("/", () =>
             {
